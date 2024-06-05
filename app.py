255a429043a16304dd4ecfa062c377fbc92f63da
Miguel Angel Valdez Navarrete <55571730+Mixcoatl120@users.noreply.github.com>
2024-06-05 15:15:20 -0600

    05/06/2024
    inicio de aplicaciones simples en proyecto , usando Python para las operaciones

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 ﻿from flask import Flask, redirect,url_for
 from CV.cv_routes import *
 from info.info_routes import *
+from ejemplos.suma_simple.ss_routes import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] =  'Hola'
@@ -12,6 +13,7 @@ def index():
 #registro de blueprints
 app.register_blueprint(cv) #carpeta de cv
 app.register_blueprint(info) # Carpeta de información
+app.register_blueprint(ss) # carpeta de ejemplos sumas simples
 
 if __name__ == '__main__':
     app.run()
---
+++ ejemplos/suma_simple/ss_routes.py
@@ -0,0 +1,16 @@
+﻿from flask import Blueprint, redirect,render_template, request,url_for,flash
+
+ss = Blueprint('ss',__name__,template_folder='templates')
+
+@ss.route("/ss",methods=['GET'])
+def SS():      
+    return render_template('ss.html')
+    
+@ss.route("/operaciones",methods=['POST','GET'])
+def Op():
+    if request.method == 'POST':
+        a = int(request.form['a'])
+        b = int(request.form['b'])
+        c = str(a+b)
+        flash(f'La suma es: {c}', 'success')  # Mensaje de éxito
+        return redirect(url_for('ss.SS',a=a,b=b,c=c))
